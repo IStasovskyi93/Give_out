e1aabc3deb12021a34801cb59b3b20dd4e67b9ca
Ihor_Stasovskyi <stsovsky400m@gmail.com>
2022-04-17 23:23:05 +0200

    cretae models

--- charity/models.py
+++ charity/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=60, verbose_name='Kategoria')
+
+    def __str__(self):
+        return self.name
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Instytucja')
+    description = models.TextField()
+    type = models.CharField(verbose_name='Wybierz typ', choices=(('fundacja', 'Fundacja'),
+                            ('organ. pazarządowa', 'Organizacja pozarządowa'), ('zbiórka lokalna', 'Zbiórka lokalna')),
+                            default='Fundacja', max_length=100)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.name
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=150)
+    phone_number = models.PositiveSmallIntegerField()
+    city = models.CharField(max_length=60)
+    zip_code = models.IntegerField()
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
+
+
